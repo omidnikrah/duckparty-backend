f66f3dc6b3efd58fbdd7da5be2a057167c741da6
Omid Nikrah <omidnikrah@gmail.com>
2025-10-08 23:13:19 +0330

    🗃️ fix: change accessories to appearance in the Duck model

--- internal/model/duck.go
+++ internal/model/duck.go
@@ -1,17 +1,21 @@
 package model
 
 import (
-	"gorm.io/datatypes"
 	"gorm.io/gorm"
 )
 
+type DuckAppearance struct {
+	Skin        string   `json:"skin"`
+	Accessories []string `json:"accessories"`
+}
+
 type Duck struct {
 	gorm.Model
-	OwnerID     uint           `json:"owner_id" gorm:"not null;index"`
-	Owner       User           `json:"owner" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Name        string         `json:"name" gorm:"not null"`
-	X           float64        `json:"x" gorm:"not null"`
-	Y           float64        `json:"y" gorm:"not null"`
-	Accessories datatypes.JSON `gorm:"type:jsonb;not null"`
-	LikesCount  int64          `gorm:"not null;default:0"`
+	OwnerID    uint           `json:"owner_id" gorm:"not null;index"`
+	Owner      User           `json:"owner" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Name       string         `json:"name" gorm:"not null"`
+	X          float64        `json:"x" gorm:"not null"`
+	Y          float64        `json:"y" gorm:"not null"`
+	Appearance DuckAppearance `json:"appearance" gorm:"serializer:json;type:jsonb;not null"`
+	LikesCount int64          `json:"likes_count" gorm:"not null;default:0"`
 }
